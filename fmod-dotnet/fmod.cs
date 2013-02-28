ef7ffdc7d988d5561222053a8ac692d2de01e642
Latif Khalifa <latifer@streamgrid.net>
2013-02-28 15:37:49 +0100

    Let us worry about system architecture

--- fmod-dotnet/fmod.cs
+++ fmod-dotnet/fmod.cs
@@ -1687,32 +1687,6 @@ namespace FMOD
     {        
         public static RESULT System_Create(ref System system)
         {
-#if WIN64
-            if (IntPtr.Size != 8)
-            {
-                /* Attempting to use 64-bit FMOD dll with 32-bit application.*/
-            
-                return RESULT.ERR_FILE_BAD;
-            }
-#else
-            if (IntPtr.Size != 4)
-            {
-                /* Attempting to use 32-bit FMOD dll with 64-bit application. A likely cause of this error 
-                 * is targetting platform 'Any CPU'. You cannot link to unmanaged dll with 'Any CPU'
-                 * target. 
-                 * 
-                 * For 32-bit applications: set the platform to 'x86'.
-                 * 
-                 * For 64-bit applications:
-                 * 1. set the platform to x64
-                 * 2. add the conditional complication symbol WIN64
-                 * 3. download the win64 fmod release
-                 * 4. copy the fmodex64.dll to the location of the .exe file for your application */
-
-                return RESULT.ERR_FILE_BAD;
-            }
-#endif
-
             RESULT result           = RESULT.OK;
             IntPtr      systemraw   = new IntPtr();
             System      systemnew   = null;
